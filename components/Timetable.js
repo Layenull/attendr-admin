08e3c4b669ac6fc57662543a6b014ced1c99eb7c
layenull <ayomidela14@gmail.com>
2023-08-22 01:03:06 +0100

    modified the time table

--- components/Timetable.js
+++ components/Timetable.js
@@ -2,27 +2,54 @@ import React from 'react'
 
 const Timetable = ({ timetableData }) => {
     return (
+        // <div>
+        //     <table className="border-collapse">
+        //         <thead>
+        //             <tr>
+        //                 <th className="border border-gray-400 p-2">Time</th>
+        //                 <th className="border border-gray-400 p-2">Monday</th>
+        //                 <th className="border border-gray-400 p-2">Tuesday</th>
+        //                 <th className="border border-gray-400 p-2">Wednesday</th>
+        //                 <th className="border border-gray-400 p-2">Thursday</th>
+        //                 <th className="border border-gray-400 p-2">Friday</th>
+        //             </tr>
+        //         </thead>
+        //         <tbody>
+        //             {timetableData.map((timeSlot) => (
+        //                 <tr key={timeSlot.time}>
+        //                     <td className="border border-gray-400 p-2">{timeSlot.time}</td>
+        //                     <td className="border border-gray-400 p-2">{timeSlot.monday}</td>
+        //                     <td className="border border-gray-400 p-2">{timeSlot.tuesday}</td>
+        //                     <td className="border border-gray-400 p-2">{timeSlot.wednesday}</td>
+        //                     <td className="border border-gray-400 p-2">{timeSlot.thursday}</td>
+        //                     <td className="border border-gray-400 p-2">{timeSlot.friday}</td>
+        //                 </tr>
+        //             ))}
+        //         </tbody>
+        //     </table>
+        // </div>
+
         <div>
-            <table className="border-collapse">
+            <table className="border-collapse w-2/3"> {/* Adjust the width as needed */}
                 <thead>
                     <tr>
-                        <th className="border border-gray-400 p-2">Time</th>
-                        <th className="border border-gray-400 p-2">Monday</th>
-                        <th className="border border-gray-400 p-2">Tuesday</th>
-                        <th className="border border-gray-400 p-2">Wednesday</th>
-                        <th className="border border-gray-400 p-2">Thursday</th>
-                        <th className="border border-gray-400 p-2">Friday</th>
+                        <th className="border border-gray-400 p-1 text-sm">Time</th>
+                        <th className="border border-gray-400 p-1 text-sm">Monday</th>
+                        <th className="border border-gray-400 p-1 text-sm">Tuesday</th>
+                        <th className="border border-gray-400 p-1 text-sm">Wednesday</th>
+                        <th className="border border-gray-400 p-1 text-sm">Thursday</th>
+                        <th className="border border-gray-400 p-1 text-sm">Friday</th>
                     </tr>
                 </thead>
                 <tbody>
                     {timetableData.map((timeSlot) => (
                         <tr key={timeSlot.time}>
-                            <td className="border border-gray-400 p-2">{timeSlot.time}</td>
-                            <td className="border border-gray-400 p-2 w-36">{timeSlot.monday}</td>
-                            <td className="border border-gray-400 p-2">{timeSlot.tuesday}</td>
-                            <td className="border border-gray-400 p-2">{timeSlot.wednesday}</td>
-                            <td className="border border-gray-400 p-2">{timeSlot.thursday}</td>
-                            <td className="border border-gray-400 p-2">{timeSlot.friday}</td>
+                            <td className="border border-gray-400 p-1 text-sm">{timeSlot.time}</td>
+                            <td className="border border-gray-400 p-1 text-sm">{timeSlot.monday}</td>
+                            <td className="border border-gray-400 p-1 text-sm">{timeSlot.tuesday}</td>
+                            <td className="border border-gray-400 p-1 text-sm">{timeSlot.wednesday}</td>
+                            <td className="border border-gray-400 p-1 text-sm">{timeSlot.thursday}</td>
+                            <td className="border border-gray-400 p-1 text-sm">{timeSlot.friday}</td>
                         </tr>
                     ))}
                 </tbody>
--- pages/schedule/cardDetails/Famas.js
+++ pages/schedule/cardDetails/Famas.js
@@ -5,64 +5,64 @@ const timetableData = [
     {
         time: '08:00 AM - 09:00 AM',
         monday: '(CSC 421) (BIO 234) (CHM 425) (VGC 234) (ABC 334) (TXT 456)',
-        tuesday: 'Subject B',
-        wednesday: 'Subject C',
-        thursday: 'Subject D',
-        friday: 'Subject E',
+        tuesday: '(CSC 421) (BIO 234) (CHM 425) (VGC 234) (ABC 334) ',
+        wednesday: '(CSC 421) (BIO 234) ',
+        thursday: '(CSC 421) (BIO 234) (CHM 425) (VGC 234) (ABC 334) (TXT 456)',
+        friday: '(CSC 421) (BIO 234) (CHM 425) (VGC 234) ',
     },
 
     {
         time: '09:00 AM - 10:00 AM',
-        monday: 'Subject F',
-        tuesday: 'Subject G',
-        wednesday: 'Subject H',
-        thursday: 'Subject I',
-        friday: 'Subject J',
+        monday: '(CHM 425) (VGC 234) (ABC 334) (TXT 456)',
+        tuesday: '(CSC 421) (BIO 234) (CHM 425) (ABC 334) (TXT 456)',
+        wednesday: '(CSC 421)  (CHM 425) (VGC 234) (ABC 334) (TXT 456)',
+        thursday: '(CSC 421) (BIO 234) (CHM 425)  (TXT 456)',
+        friday: '(CSC 421) (BIO 234) ) (ABC 334) (TXT 456)',
     },
 
     {
-        time: '09:00 AM - 10:00 AM',
-        monday: 'Subject F',
-        tuesday: 'Subject G',
-        wednesday: 'Subject H',
-        thursday: 'Subject I',
-        friday: 'Subject J',
+        time: '10:00 AM - 12:00 PM',
+        monday: '(CSC 421) (BIO 234) (CHM 425) (VGC 234) (ABC 334) (TXT 456)',
+        tuesday: '(CSC 421) (TXT 456)',
+        wednesday: '(CSC 421)  (CHM 425) (VGC 234) (ABC 334) (TXT 456)',
+        thursday: '(CSC 421) (BIO 234) (CHM 425) (VGC 234) (ABC 334) (TXT 456)',
+        friday: '(CSC 421) (ABC 334) (TXT 456)',
     },
 
     {
-        time: '09:00 AM - 10:00 AM',
-        monday: 'Subject F',
-        tuesday: 'Subject G',
-        wednesday: 'Subject H',
-        thursday: 'Subject I',
-        friday: 'Subject J',
+        time: '12:00 PM - 01:00 PM',
+        monday: 'B',
+        tuesday: 'R',
+        wednesday: 'E',
+        thursday: 'A',
+        friday: 'K',
     },
 
     {
-        time: '09:00 AM - 10:00 AM',
-        monday: 'Subject F',
-        tuesday: 'Subject G',
-        wednesday: 'Subject H',
-        thursday: 'Subject I',
-        friday: 'Subject J',
+        time: '01:00 PM - 3:00 PM',
+        monday: ' (VGC 234) (ABC 334) (TXT 456)',
+        tuesday: '(CSC 421) (BIO 234) (CHM 425) (VGC 234) (ABC 334) (TXT 456)',
+        wednesday: '(CHM 425) (VGC 234) (ABC 334) (TXT 456)',
+        thursday: '(CSC 421) (BIO 234) (CHM 425) (VGC 234) (ABC 334) (TXT 456)',
+        friday: '(CSC 421)  (VGC 234) (ABC 334) (TXT 456)',
     },
 
     {
-        time: '09:00 AM - 10:00 AM',
-        monday: 'Subject F',
-        tuesday: 'Subject G',
-        wednesday: 'Subject H',
-        thursday: 'Subject I',
-        friday: 'Subject J',
+        time: '03:00 PM - 4:00 AM',
+        monday: '(CSC 421)  (VGC 234) (ABC 334) (TXT 456)',
+        tuesday: '(CSC 421) (BIO 234) (CHM 425) (VGC 234) (ABC 334) (TXT 456)',
+        wednesday: '(CSC 421) (BIO 234) (CHM 425) (VGC 234) (ABC 334) (TXT 456)',
+        thursday: '(CSC 421)  (VGC 234) (ABC 334) (TXT 456)',
+        friday: '(CSC 421) (BIO 234) (CHM 425) ',
     },
 
     {
-        time: '09:00 AM - 10:00 AM',
-        monday: 'Subject F',
-        tuesday: 'Subject G',
-        wednesday: 'Subject H',
-        thursday: 'Subject I',
-        friday: 'Subject J',
+        time: '04:00 AM - 05:00 AM',
+        monday: '(CSC 421) (BIO 234) (CHM 425)  (ABC 334) (TXT 456)',
+        tuesday: '(CSC 421)  (VGC 234) (ABC 334) (TXT 456)',
+        wednesday: '(CSC 421) (BIO 234) (CHM 425) (VGC 234) (ABC 334) (TXT 456)',
+        thursday: '(CSC 421) (BIO 234) (CHM 425) (VGC 234) ',
+        friday: '',
     },
     // Add more time slots and subject data as needed here.
 ];
@@ -71,7 +71,10 @@ const Famas = () => {
     return (
         <div className='flex flex-col items-center space-y-10 justify-center'>
             <h1 className=' font-extrabold text-4xl pl-32 pt-6'>Timetable of the Faculty of Art, Management and Social Sciences</h1>
-            <Timetable timetableData={timetableData} />
+            <div className=' relative left-72'>
+                <Timetable timetableData={timetableData} />
+
+            </div>
         </div>
     )
 }
